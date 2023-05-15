673b1d18a4c1e43ced57075fd670471c87088279
guemax <github-mg@guenther.cloud>
2023-05-15 14:30:31 +0200

    Extract if statement for modifying the search text

--- modify_search_text/__init__.py
+++ modify_search_text/__init__.py
@@ -22,24 +22,34 @@ def modify_search_text(context: SearchContext) -> None:
     if context.ids:
         return
 
-    search_text_modification_disabled_command = settings.read('disable_modification_of_search_text_by_addon_command')
-    if is_more_than_one_word(context.search) \
-            or search_text_modification_disabled_by_user(context.search, search_text_modification_disabled_command) \
-            or custom_search_filter_has_been_applied_by_anki_or_by_user(context.search):
-        context.search = context.search.replace(search_text_modification_disabled_command, "")
+    if should_not_modify_search_text(context.search):
+        context.search = remove_disable_command_from_search_text_if_necessary(context.search)
         return
 
     context.search = replace_vowels.replace_vowels_in_search_text(context.search)
     context.search = f"nc:{context.search}"
 
 
+def should_not_modify_search_text(search: str) -> bool:
+    return is_more_than_one_word(search) \
+        or search_text_modification_disabled_by_user(search) \
+        or advanced_search_filter_has_been_applied(search)
+
+
 def is_more_than_one_word(search: str) -> bool:
     return len(search.split(" ")) > 1
 
 
-def custom_search_filter_has_been_applied_by_anki_or_by_user(search: str) -> re.Match:
+def advanced_search_filter_has_been_applied(search: str) -> re.Match:
+    # Matches strings like `id:123', `nid:123', `nc:bonjour', ...
     return re.search(r'\w+:\w+', search)
 
 
-def search_text_modification_disabled_by_user(search: str, search_text_modification_disabled_command: str) -> bool:
-    return search_text_modification_disabled_command in search
+def search_text_modification_disabled_by_user(search: str) -> bool:
+    disable_command = settings.read('disable_modification_of_search_text_by_addon_command')
+    return disable_command in search
+
+
+def remove_disable_command_from_search_text_if_necessary(search: str) -> str:
+    disable_command = settings.read('disable_modification_of_search_text_by_addon_command')
+    return search.replace(disable_command, "")
